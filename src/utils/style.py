f9bc174acfff30edff08ca504667060b7753d64e
Luke Murray <120339106@umail.ucc.ie>
2026-05-14 11:56:00 +0100

    Colour and formatting for plots

--- src/utils/style.py
+++ src/utils/style.py
@@ -1,1 +1,40 @@
-# style
+# Colour scheme and general formatting for plotting throughout the thesis
+
+METHOD_COLOURS = {
+    "FTE": "#4477AA", # Blue
+    "HH": "#228833", # Green
+    "ProjEuler": "#AA3377", # Magenta
+    "KL": "#EE6677", # Salmon
+    "ChoiKwok": "#AA8822", # Gold(ish)
+    "Exact": "#000000", # Black
+}
+
+METHOD_LINESTYLES = {
+    "FTE" : "-",
+    "HH": "--",
+    "ProjEuler": "-.",
+    "KL": ":",
+    "ChoiKwok": "--",
+    "Exact": "-"
+}
+
+REGIME_COLOURS = {
+    "A": "#0072B2", # Feller well-satisified
+    "B": "#56B4E9", # Feller satisfied
+    "C": "#CCBB44", # Feller Boundary
+    "D": "#E69F00", # Feller violated
+    "E": "#D55E00", # Stong Feller violation
+}
+
+METHOD_LABELS = {
+    "FTE": "Full Truncation Euler",
+    "HH": "H-H Milstein",
+    "ProjEuler": "Projected Euler",
+    "KL": "Kelly-Lord",
+    "ChoiKwok": "Choi-Kwok",
+    "Exact": "Exact",
+}
+
+DEFAULT_LINEWDITH = 1.8
+EXACT_LINEWIDTH = 2.2
+GRID_ALPHA = 0.25
