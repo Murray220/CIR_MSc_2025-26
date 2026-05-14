a7035c3383d7da7f2954887688d233615bcb712e
Luke Murray <120339106@umail.ucc.ie>
2026-05-14 14:06:55 +0100

    removed CIR parameters into seperate file

--- src/utils/style.py
+++ src/utils/style.py
@@ -1,14 +1,3 @@
-# CIR parameters (temporary here for now 14.05.2026)
-
-def cir_delta(kappa: float, theta: float, sigma: float) -> float:
-    return 4.0 * kappa * theta / sigma**2
-
-
-def kl_alpha(kappa: float, theta: float, sigma: float) -> float:
-    return (4.0 * kappa * theta - sigma**2) / 8.0
-
-
-
 # Colour scheme and general formatting for plotting throughout the thesis
 
 METHOD_COLOURS = {
