086ce0f6e3eade73920ee949876342cd8fb4b8ec
Luke Murray <120339106@umail.ucc.ie>
2026-05-18 14:50:14 +0100

    Fixes to brownian sampler

--- src/utils/brownian.py
+++ src/utils/brownian.py
@@ -6,16 +6,17 @@ def aggregate_brownian_increments(
         factor: int,
 ) -> np.ndarray:
     
-    if factor <0 :
+    if factor <= 0:
         raise ValueError("factor must be positive")
+    
     if dW_fine.ndim != 2:
         raise ValueError("dW_fine needs to be a 2D array")
-    
-    n_paths, n_fine_steps = dW_fine.shape
 
-    n_coarse_steps = n_fine_steps // factor
+    n_paths, n_fine_steps = dW_fine.shape
 
     if n_fine_steps % factor != 0:
         raise ValueError("number of fine steps must be divisible by chosen factor")
+
+    n_coarse_steps = n_fine_steps // factor
 
     return dW_fine.reshape(n_paths, n_coarse_steps, factor).sum(axis = 2)
